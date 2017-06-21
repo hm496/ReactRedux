918397d8b6850ff79bfdd88a4ae9579b1b2293f3
hm496 <hm496@163.com>
2017-06-21 18:05:27 +0800

    修改

--- one-app/src/app.js
+++ one-app/src/app.js
@@ -1,4 +1,5 @@
 import React, {Component, cloneElement} from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
 
@@ -17,6 +18,31 @@ class Tabs extends Component {
         this.setState({
             count: this.state.count + 1,
         });
+    }
+
+    //由于父组件更新 props 而更新
+    //此方法可以作为 React 在 props 传入后，渲染之前 setState 的
+    //机会。在此方法中调用 setState 是不会二次渲染的
+    componentWillReceiveProps(nextProps) {
+        // this.setState({})
+        // console.log(arguments);
+    }
+
+    //是否 现在更新组件
+    shouldComponentUpdate(nextProps, nextState) {
+        // console.log(arguments);
+        return true;
+    }
+
+    //将要 更新组件
+    componentWillUpdate(nextProps, nextState) {
+        // console.log(arguments);
+    }
+
+    //完成 更新组件
+    componentDidUpdate(prevProps, prevState) {
+        //上一个属性,上一个状态
+        // this 为当前组件的实例
     }
 
     render() {
