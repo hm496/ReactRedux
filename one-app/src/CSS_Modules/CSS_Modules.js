508e7266272e482623601282358c49f58b18a612
hm496 <hm496@163.com>
2017-06-26 14:22:29 +0800

    CSS Modules 结合 React 实践

---
+++ one-app/src/CSS_Modules/CSS_Modules.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Administrator on 2017/6/26.
+ */
+/* components/Button.js */
+import React, {Component} from 'react';
+import classNames from 'classnames';
+import CSSModules from 'react-css-modules';
+import styles from './Button.scss';
+
+class Dialog extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            disabled: false
+        }
+    }
+
+    render() {
+        const cx = classNames({
+            confirm: !this.state.disabled,
+            disabledConfirm: this.state.disabled,
+        });
+        return (
+            <div styleName="root">
+                <a styleName={cx}>Confirm</a>
+            </div>
+        );
+    }
+}
+
+export default CSSModules(Dialog, styles);
+// =>
+// Object {
+// normal: 'button--normal-abc5436',
+// disabled: 'button--disabled-def884',
+// }
