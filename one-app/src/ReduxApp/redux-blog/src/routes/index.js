85615efe6d4d0cc956dc7ea556a4069a437e73a6
hm496 <hm496@163.com>
2017-09-08 10:36:12 +0800

    路由结构

--- one-app/src/ReduxApp/redux-blog/src/routes/index.js
+++ one-app/src/ReduxApp/redux-blog/src/routes/index.js
@@ -1,18 +1,19 @@
 // routes/index.js
 import React from 'react';
-import { Router, Route, HashRouter } from 'react-router-dom';
-import Home from '../views/Home';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import Frame from '../views/Frame'
+import Home from '../views/Home'
 import Detail from '../views/Detail'
 
 const routes = (
-  <HashRouter
-    basename="/"
-  >
-    <div>
-      <Route exact path="/" component={Home}/>
-      <Route path="/detail/:id" component={Detail}/>
-    </div>
-  </HashRouter>
+  <Router>
+    <Route path="/">
+      <Frame>
+        <Route exact path="/" component={Home}/>
+        <Route path="/detail/:id" component={Detail}/>
+      </Frame>
+    </Route>
+  </Router>
 );
 
 export default routes;
--- one-app/src/ReduxApp/redux-blog/src/views/Detail.js
+++ one-app/src/ReduxApp/redux-blog/src/views/Detail.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class Detail extends Component {
   render() {
     return (
-      <h1>Detail</h1>
+      <div>
+        <h1>Detail</h1>
+      </div>
     )
   };
 }
